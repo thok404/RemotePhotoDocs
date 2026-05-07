cd3112b1878e4103839fe99a0a5028c09ce6d7e0
thok404 <109383521+thok404@users.noreply.github.com>
2026-05-07 21:15:59 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,19 +29,7 @@ export default defineConfig({
 				},
 			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			
 		}),
 	],
 });
